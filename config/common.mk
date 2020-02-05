0290596dc63fbcaa973893914df2250f55c5703a
Wang Han <416810799@qq.com>
2020-02-05 16:45:15 +0800

    backuptool: Remove backup blacklist/whitelist
    Change-Id: Ia89e6fa4c31377a429e0dfa912d0b61393696009
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -37,8 +37,7 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/derp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/derp/prebuilt/common/bin/50-base.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-base.sh \
-    vendor/derp/prebuilt/common/bin/blacklist:$(TARGET_COPY_OUT_SYSTEM)/addon.d/blacklist
+    vendor/derp/prebuilt/common/bin/50-base.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-base.sh
 
 ifneq ($(AB_OTA_PARTITIONS),)
 PRODUCT_COPY_FILES += \
