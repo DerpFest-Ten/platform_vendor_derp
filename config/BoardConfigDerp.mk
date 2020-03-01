4b6bf850e2aa2a9f60e4c2e4e470c3fec970f7bc
Nolen Johnson <johnsonnolen@gmail.com>
2020-03-01 14:31:48 -0500

    derp: configs: Flatten APEX(s) on official builds
    * APEX is useless to average Lineage user, as we aren't
      licensed to ship the GMS APEX's, nor would we want to,
      as we want to keep compatibility with non-GMS use-cases.
    
    * Additionally, APEX's should each be signed with their
      own unique key, and number of them can vary per-device,
      which makes signing them in a standard way on infrastructure
      quite complex.
    
    * Though this flag is default, we need to disable it
      explicitly to make sure any maintainers who set up the
      configurations for APEX images won't change needs of
      infrastructure. This will allow people who want to build
      locally to use full APEX(s).
    
    Change-Id: I7f2eb52593eeca1b3761a7a163ce9079995870c6
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/BoardConfigDerp.mk
+++ config/BoardConfigDerp.mk
@@ -1,3 +1,8 @@
+# APEX
+ifneq ($(filter Official CI Private,$(DERP_BUILDTYPE)),)
+    TARGET_FLATTEN_APEX := true
+endif
+
 include vendor/derp/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
